fa3577252273cc605acb05e2eb817129e5dc709b
Furkan Alioglu <91939823+furkanalioglu@users.noreply.github.com>
2023-10-08 21:48:33 +0300

    Before Youtube API and FileManager

---
+++ awsPrimeVideoClone/awsPrimeVideoClone/UI/Home/HomeCell/HomeDetails/HomeDetailsHeader/HomeDetailsHeader.swift
@@ -0,0 +1,20 @@
+//
+//  HomeDetailsHeader.swift
+//  awsPrimeVideoClone
+//
+//  Created by Furkan Alioglu on 8.10.2023.
+//
+
+import UIKit
+
+class HomeDetailsHeader: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
